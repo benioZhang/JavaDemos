23867bbefc8a0981cb33307e5e85d9ad7dcbb780
zhangzhibin <314053215@qq.com>
2019-06-16 17:09:08 +0800

    update leetcode#349 solution

--- src/main/java/leetcode/hash/HashSolutions.java
+++ src/main/java/leetcode/hash/HashSolutions.java
@@ -88,7 +88,7 @@ public class HashSolutions {
     public static int[] intersection2(int[] nums1, int[] nums2) {
         Arrays.sort(nums1);
         Arrays.sort(nums2);
-        int[] intersection = new int[nums2.length];
+        int[] intersection = new int[Math.min(nums1.length, nums2.length)];
         int i = 0, j = 0, k = 0;
         while (i < nums1.length && j < nums2.length) {
             if (nums1[i] == nums2[j]) {
